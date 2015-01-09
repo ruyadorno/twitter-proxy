04f2590d9d53c239c25496b1ffa40d77983bd1a6
Ruy Adorno <ruyadorno@hotmail.com>
2015-01-08 23:46:57 -0500

    Added Content-Type to requests

--- server.js
+++ server.js
@@ -37,6 +37,7 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header('Content-Type', 'application/javascript');
   if (req.method === 'OPTIONS') return res.send(200);
   next();
 });
