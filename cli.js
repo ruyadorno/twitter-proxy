3002c1b96374dba7003bd4e6d93aeffd3007b041
Acsor <nildexo@yandex.com>
2020-10-27 23:47:55 +0100

    Fix cli.js bug preventing correct path argument interpretation

--- cli.js
+++ cli.js
@@ -1,17 +1,17 @@
 #!/usr/bin/env node
 
-var path = require('path');
+const path = require('path');
 
 // Grab the config file
-var configPath = process.argv[2] || './config.json';
-var config;
+let configPath = process.argv[2] || path.join(process.cwd(), './config.json');
+let config;
 
 try {
-  config = require(path.join(process.cwd(), configPath));
+    config = require(configPath);
 } catch (e) {
-  console.error("No config file found.");
-  process.exit(1);
+    console.error("No config file found.");
+    console.error(e);
+    process.exit(1);
 }
 
 require('./server')(config);
-
